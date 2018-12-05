20c024469bd15b83019f5c2f54785f801b0896b8
fanxingguang <fanxingguang@megvii.com>
2018-12-05 17:28:13 +0800

    add

---
+++ src/main/java/com/my/boot/MetaApplication.java
@@ -0,0 +1,12 @@
+package com.my.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaApplication.class, args);
+	}
+}
